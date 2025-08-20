3c45abf58968fa5a1325b55cd2f1508ca8e397cd
k15r <git@riddance.k15r.xyz>
2025-08-20 17:56:59 +0200

    Node update.

--- test/hello.ts
+++ test/hello.ts
@@ -1,12 +1,17 @@
-import { request } from '@riddance/service/test/http'
+import { request, withBearer } from '@riddance/service/test/http'
 import assert from 'node:assert/strict'
 
 describe('hello', () => {
     it('should greet', async () => {
-        const response = await request({
-            uri: '/',
-        })
+        const response = await request(
+            await withBearer(
+                {},
+                {
+                    uri: '/',
+                },
+            ),
+        )
         assert.deepStrictEqual(response.status, 200)
-        assert.deepStrictEqual(response.body.message, 'Hello from Riddance!')
+        assert.deepStrictEqual(response.body.message, 'Hello from Riddance, World!')
     })
 })
